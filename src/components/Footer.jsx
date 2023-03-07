9cc08a3e1dba3cef05970ccb7b1c193155dc1995
george_beyrouti <georges.beyrouti@gmail.com>
2023-03-07 22:47:37 +0100

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -69,7 +69,7 @@ const Footer = () => {
       <div className='  max-w-screen-full w-full bg-gray-800 text-white lg:static  px-2 sticky bottom-0  pt-2 '>
         <div className='max-w-screen flex flex-row py-1 mx-auto justify-around items-center'>
             <div>
-              <div className=" lg:text-lg md:text-md sm:text-sm text-xs">Designed by</div>
+              <div className=" lg:text-lg md:text-md sm:text-sm text-xs">Designed by:</div>
               <div className=" lg:text-lg md:text-md sm:text-sm text-xs">George Beyrouti.</div>
               <div className=" lg:text-lg md:text-md sm:text-sm text-xs my-1">Using React & Tailwind</div> 
             </div>
